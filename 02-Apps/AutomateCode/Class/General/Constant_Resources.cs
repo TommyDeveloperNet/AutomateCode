f1e3271d46aeecabf0e2b0491c7861f765570c40
Tomás Fernández Sánchez <tommy.developer@outlook.es>
2021-10-16 20:01:06 +0200

    Configuración aplicacion frontal AutomateCode

---
+++ 02-Apps/AutomateCode/Class/General/Constant_Resources.cs
@@ -0,0 +1,8 @@
+﻿namespace AutomateCode.Class.General
+{
+    class Constant_Resources
+    {
+        public static string Resources { get => string.Format("{0}.Resources", Constant_config_App.NameProject); }
+        public static string ResourceMsg { get => string.Format("{0}.ResourcesMsg", Constant_config_App.NameProject); }
+    }
+}
---
+++ 02-Apps/AutomateCode/Class/General/MsgApp.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Resources;
+
+namespace AutomateCode.Class.General
+{
+    class MsgApp
+    {
+    }
+}
